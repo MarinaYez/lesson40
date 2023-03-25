15a2aaf687fc5477b41a17025aba0c7c3648143d
MarinaYez <mariosadchuk@gmail.com>
2023-03-26 00:08:59 +0200

    commit

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ArticleAuthor from "./ArticleAuthor";
+import ArticleBody from "./ArticleBody";
+
+function Article({children, show, lang}) {
+  
+    return (
+    <>
+    {children}
+        <ArticleBody show={show} lang={lang} />
+    <ArticleAuthor lang={lang}/> 
+    </>
+    );
+  }
+  export default Article;
+
+
